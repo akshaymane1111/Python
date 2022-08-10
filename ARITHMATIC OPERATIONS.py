3631dd2c6a5d151f258adae443f74012d2895260
akshaymane1111 <68591354+akshaymane1111@users.noreply.github.com>
2022-08-10 20:56:17 +0530

    Add files via upload

---
+++ ARITHMATIC OPERATIONS.py
@@ -0,0 +1,21 @@
+# Store input numbers:
+A = input('Enter first number: ')
+B= input('Enter second number: ')
+
+# Add two numbers
+sum = float(A) + float(B)
+# Subtract two numbers
+min = float(A) - float(B)
+# Multiply two numbers
+mul = float(A) * float(B)
+# Divide two numbers
+div = float(A) / float(B)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(A, B, sum))
+
+# Display the subtraction
+print('The subtraction of {0} and {1} is {2}'.format(A, B, min))
+# Display the multiplication
+print('The multiplication of {0} and {1} is {2}'.format(A, B, mul))
+# Display the division
+print('The division of {0} and {1} is {2}'.format(A, B, div))
